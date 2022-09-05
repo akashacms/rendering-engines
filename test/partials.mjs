225f4b25dc87734774a54432ccc2be8dd013cc2d
David Herron <david@davidherron.com>
2022-09-05 15:48:12 -0700

    Complex partials w/ include

--- test/partials.mjs
+++ test/partials.mjs
@@ -14,10 +14,12 @@ import { Configuration } from '../dist/index.js';
 // import { describe, it } from 'node:test';
 
 const config = new Configuration({
-    partialDirs: [ path.join(__dirname, 'partials1') ],
+    partialDirs: [ 
+        path.join(__dirname, 'partials1'),
+        path.join(__dirname, 'partials-include')
+    ],
     layoutDirs:  [ path.join(__dirname, 'layouts1') ]
 });
-
 
 describe('EJS', function() {
 
@@ -48,6 +50,39 @@ describe('EJS', function() {
         assert.ok(rendered);
         assert.match(rendered, /.*Partial Body Text.*/);
     });
+
+    it('should render EJS including.html.ejs', async function() {
+        let rendered;
+        try {
+            rendered = await config.partial('including.html.ejs', {
+                message: 'Hello, World',
+                includedStuff: 'This is included'
+            });
+        } catch (e) {
+            console.error(e);
+            rendered = undefined;
+        }
+        assert.ok(rendered);
+        assert.match(rendered, /.span..p.This is included..p...span/);
+        assert.match(rendered, /.div.Hello, World..div./);
+    });
+
+    it('should render Sync EJS including.html.ejs', function() {
+        let rendered;
+        try {
+            rendered = config.partialSync('including.html.ejs', {
+                message: 'Hello, World',
+                includedStuff: 'This is included'
+            });
+        } catch (e) {
+            console.error(e);
+            rendered = undefined;
+        }
+        assert.ok(rendered);
+        assert.match(rendered, /.span..p.This is included..p...span/);
+        assert.match(rendered, /.div.Hello, World..div./);
+    });
+
 });
 
 describe('Liquid', function() {
@@ -80,6 +115,24 @@ describe('Liquid', function() {
         assert.ok(typeof rendered === 'undefined' || rendered === null);
         assert.ok(caughtError === true);
     });
+
+    it('should render Liquid including.html.liquid', async function() {
+        let rendered;
+        try {
+            rendered = await config.partial('including.html.liquid', {
+                message: 'Hello, World',
+                includedStuff: 'This is included'
+            });
+        } catch (e) {
+            console.error(e);
+            rendered = undefined;
+        }
+        assert.ok(rendered);
+        assert.match(rendered, /.span..p.This is included..p...span/);
+        assert.match(rendered, /.div.Hello, World..div./);
+    });
+
+    // Liquid not supported for Sync rendering
 });
 
 describe('Nunjucks', function() {
@@ -111,6 +164,39 @@ describe('Nunjucks', function() {
         assert.ok(rendered);
         assert.match(rendered, /.*Partial Body Text.*/);
     });
+
+    it('should render Nunjucks including.html.njk', async function() {
+        let rendered;
+        try {
+            rendered = await config.partial('including.html.njk', {
+                message: 'Hello, World',
+                includedStuff: 'This is included'
+            });
+        } catch (e) {
+            console.error(e);
+            rendered = undefined;
+        }
+        assert.ok(rendered);
+        assert.match(rendered, /.span..p.This is included..p...span/);
+        assert.match(rendered, /.div.Hello, World..div./);
+    });
+
+    it('should render Sync Nunjucks including.html.njk', function() {
+        let rendered;
+        try {
+            rendered = config.partialSync('including.html.njk', {
+                message: 'Hello, World',
+                includedStuff: 'This is included'
+            });
+        } catch (e) {
+            console.error(e);
+            rendered = undefined;
+        }
+        assert.ok(rendered);
+        assert.match(rendered, /.span..p.This is included..p...span/);
+        assert.match(rendered, /.div.Hello, World..div./);
+    });
+
 });
 
 describe('Handlebars', function() {
@@ -142,4 +228,9 @@ describe('Handlebars', function() {
         assert.ok(rendered);
         assert.match(rendered, /.*Partial Body Text.*/);
     });
+
+    // Handlebars does not support include at all
+    // It does support partials, but they have to be preregistered,
+    // and it does not support searching for partials in the filesystem.
+    // Hence we will not test "include" with Handlebars
 });
